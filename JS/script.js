9b9a43dce4e25d9cf44717403e16120f94ad82c5
SantiagoPinilla <johansantiagopinillauribe@gmail.com>
2021-05-27 02:04:52 -0500

    Beautifying code with VS Code Extension

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,13 @@
 let flagBackground = false;
 
 function btnFunction() {
+
     var btnHelloWorld = document.getElementById('helloWorld');
 
-    if (flagBackground){
+    if (flagBackground) {
         flagBackground = false;
         btnHelloWorld.style.cssText = 'color: #808000; background-color: #f3f3f3; font-weight: bold;'
-    }
-    else {
+    } else {
         flagBackground = true;
         btnHelloWorld.style.cssText = 'color: #671792; background-color: #141616; font-weight: bold;'
     }
